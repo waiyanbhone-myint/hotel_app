a1201d141938b4c0bee62a642f70a47936847d6c
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-05-08 11:36:49 -0400

    Project started

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -2,6 +2,14 @@ package com.ps;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    }
+
+    public static int appMenu(){
+        System.out.println("======Welcome to Hotel Management======");
+        System.out.println("1. Add Room");
+        System.out.println("2. Check In Guest");
+        System.out.println("3. Check Out Guest");
+        System.out.println("4. Clean Room");
     }
 }
--- src/main/java/com/ps/RoomManagement.java
+++ src/main/java/com/ps/RoomManagement.java
@@ -5,12 +5,19 @@ public class RoomManagement {
     private int roomNumber;
     private String type;
     private double payRate;
+    private boolean isOccupied = false;
+    private boolean isClean = true;
+    private String guestName = null;
+
+    //---------Constructor----------//
 
     public RoomManagement(int roomNumber, String type, double payRate) {
         this.roomNumber = roomNumber;
         this.type = type;
         this.payRate = payRate;
     }
+
+    //-------- Getter and Setter -------//
 
     public int getRoomNumber() {
         return roomNumber;
@@ -34,5 +41,56 @@ public class RoomManagement {
 
     public void setPayRate(double payRate) {
         this.payRate = payRate;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isClean() {
+        return isClean;
+    }
+
+    public void setClean(boolean clean) {
+        isClean = clean;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    //-------Derived Getter-------//
+    public boolean roomIsAvailable() {
+        return !isOccupied && isClean;
+    }
+
+
+    public void checkIn(String guestName) {
+        if (roomIsAvailable()) {
+            System.out.println("Room is available to check In.");
+            isOccupied = true;
+            isClean = false;
+        } else {
+            System.out.println("Room is taken by the guest name: " + guestName);
+        }
+
+    }
+
+    public void checkOut(){
+        if(!isOccupied){
+            System.out.println("Room is already vacant.");
+        }
+        else{
+            System.out.println("Check out steps");
+        }
+
     }
 }
